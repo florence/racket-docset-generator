12e1bd2a4c84b2e6787bf750016d7561766942aa
Spencer Florence <spencer@florence.io>
2015-12-25 10:39:49 -0500

    s/return/continue

--- generator.py
+++ generator.py
@@ -31,46 +31,71 @@ def fetch(f):
     for group in soup.select('.tocviewtitle'):
         for tag in group.select('.tocviewselflink'):
             _type = "Guide"
-            if tag['href'] == '':
+            if tag.has_attr('href') and tag['href'] == '':
                 _path = filename.replace(docpath, '')
                 _name = tag.text
                 print('type: %s, name: %s, path: %s' % (_type, _name, _path))
                 cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (_name, _type, _path))
 
 
-    for group in soup.select('.SVInsetFlow'):
+    for group in soup.select('.SIntrapara'):
         type_ele = group.find('div', { 'class': 'RBackgroundLabelInner' })
         _path = None
-
-
-        for tag in group.select('.RktStxDef'):
-            _name = tag.text
-            if tag['href'].startswith('#'):
-                _path = filename.replace(docpath, '') + tag['href']
-            elif 'local-redirect' in tag['href']:
-                matches = re.search('doc=(.*)&rel=(.*)', tag['href'])
-                _path = matches.group(1) + '/' + parse.unquote(matches.group(2))
-
-            if _path:
-                print('type: %s, name: %s, path: %s' % ("Syntax", _name, _path))
-                cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (_name, "Syntax", _path))
 
         if type_ele:
             _type = type_ele.text
         else:
-            return
+            continue
+
+        for tag in group.select('.RktValLink'):
+            _name = tag.text
+            if tag.has_attr('href'):
+                if tag['href'].startswith('#'):
+                    _path = filename.replace(docpath, '') + tag['href']
+                elif 'local-redirect' in tag['href']:
+                    matches = re.search('doc=(.*)&rel=(.*)', tag['href'])
+                    _path = matches.group(1) + '/' + parse.unquote(matches.group(2))
+
+                if _path:
+                    print('type: %s, name: %s, path: %s' % (_type, _name, _path))
+                    cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (_name, _type.title(), _path))
+
+
+
+    for group in soup.select('.SVInsetFlow'):
+        type_ele = group.find('div', { 'class': 'RBackgroundLabelInner' })
+        _path = None
+
+        for tag in group.select('.RktStxDef'):
+            _name = tag.text
+            if tag.has_attr('href'):
+                if tag['href'].startswith('#'):
+                    _path = filename.replace(docpath, '') + tag['href']
+                elif 'local-redirect' in tag['href']:
+                    matches = re.search('doc=(.*)&rel=(.*)', tag['href'])
+                    _path = matches.group(1) + '/' + parse.unquote(matches.group(2))
+
+                if _path:
+                    print('type: %s, name: %s, path: %s' % ("Syntax", _name, _path))
+                    cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (_name, "Syntax", _path))
+
+        if type_ele:
+            _type = type_ele.text
+        else:
+            continue
 
         for tag in group.select('.RktValDef'):
             _name = tag.text
-            if tag['href'].startswith('#'):
-                _path = filename.replace(docpath, '') + tag['href']
-            elif 'local-redirect' in tag['href']:
-                matches = re.search('doc=(.*)&rel=(.*)', tag['href'])
-                _path = matches.group(1) + '/' + parse.unquote(matches.group(2))
+            if tag.has_attr('href'):
+                if tag['href'].startswith('#'):
+                    _path = filename.replace(docpath, '') + tag['href']
+                elif 'local-redirect' in tag['href']:
+                    matches = re.search('doc=(.*)&rel=(.*)', tag['href'])
+                    _path = matches.group(1) + '/' + parse.unquote(matches.group(2))
 
-            if _path:
-                print('type: %s, name: %s, path: %s' % (_type, _name, _path))
-                cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (_name, _type.title(), _path))
+                if _path:
+                    print('type: %s, name: %s, path: %s' % (_type, _name, _path))
+                    cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (_name, _type.title(), _path))
 
 
 for root, dirs, files in os.walk(docpath):
